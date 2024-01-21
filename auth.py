fa671293fd204f1f353e195fb5cf1a03193443e0
Bakare Abiola <iamstarcode@gmail.com>
2024-01-21 17:58:25 +0100

    feat: optimized validate_jwt

--- auth.py
+++ auth.py
@@ -8,18 +8,21 @@ settings = Settings()
 
 
 def validate_jwt(authorization: Annotated[str, Header()]) -> str:
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=400, detail="Invalid Authorization")
+
     access_token = authorization.split(" ")[1]
 
+    if not access_token:
+        raise HTTPException(status_code=400, detail="Invalid Authorization")
+
     try:
-        if access_token is not None:
-            decoded = jwt.decode(
-                access_token,
-                settings.supabase_jwt_secret,
-                algorithms=["HS256"],
-                options={"verify_aud": False, "verify_signature": True},
-            )
-        else:
-            raise HTTPException(status_code=400, detail="Invalid Authorization")
+        jwt.decode(
+            access_token,
+            settings.supabase_jwt_secret,
+            algorithms=["HS256"],
+            options={"verify_aud": False, "verify_signature": True},
+        )
 
     except jwt.InvalidSignatureError as e:
         print(f"Error: {e}")
